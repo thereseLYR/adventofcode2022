a85ad41e5baa0647c655758ba184b4c2f753241f
thereseLYR <thereselau1902@gmail.com>
2022-12-05 02:17:01 +0800

    part 2 solution

--- day4/solution.py
+++ day4/solution.py
@@ -7,9 +7,16 @@ def generate_sections(string):
   sections = range(int(firstNum), int(lastNum) + 1)
   return sections
 
-def checkRangeInRange(r1, r2):
+def checkRangeCompleteOverlap(r1, r2):
   cond1 = r1.start in r2 and r1[-1] in r2
   cond2 = r2.start in r1 and r2[-1] in r1 
+  if cond1 or cond2 is True:
+    return True
+  else: return False
+
+def checkRangePartialOverlap(r1, r2):
+  cond1 = r1.start in r2
+  cond2 = r2.start in r1
   if cond1 or cond2 is True:
     return True
   else: return False
@@ -20,13 +27,21 @@ def part1(input):
   for pair in input:
     elf1Assignment, elf2Assignment = pair.split(',')
     elf1Range, elf2Range = generate_sections(elf1Assignment), generate_sections(elf2Assignment)
-    completeOverlap = checkRangeInRange(elf1Range, elf2Range)
+    completeOverlap = checkRangeCompleteOverlap(elf1Range, elf2Range)
     if completeOverlap == True:
       overlapCount += 1
   print(overlapCount) # 464
   return
 
 def part2(input):
+  overlapCount = 0
+  for pair in input:
+    elf1Assignment, elf2Assignment = pair.split(',')
+    elf1Range, elf2Range = generate_sections(elf1Assignment), generate_sections(elf2Assignment)
+    partialOverlap = checkRangePartialOverlap(elf1Range, elf2Range)
+    if partialOverlap == True:
+      overlapCount += 1
+  print(overlapCount) # 770
   return
 
 if __name__ == "__main__":
