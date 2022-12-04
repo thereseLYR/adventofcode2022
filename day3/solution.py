38e6ff2f91d0d71e5eff9178d37f3e46079e5e54
thereseLYR <thereselau1902@gmail.com>
2022-12-05 01:10:42 +0800

    part 2 solution

--- day3/solution.py
+++ day3/solution.py
@@ -6,10 +6,39 @@ def halveStr(str):
   firstpart, secondpart = str[:int(len(str)/2)], str[int(len(str)/2):]
   return firstpart, secondpart
 
+def groupRucksacks(input):
+  groupCounter = 0
+  allGroupsList = []
+  singleGroupList = []
+
+  # read rucksack - if current groupCounter is not maxed out, add the rucksack to the singleGroupList
+  for rucksack in input:
+    if groupCounter < 3:
+      singleGroupList.append(rucksack)
+      groupCounter += 1
+    else:
+      allGroupsList.append(singleGroupList)
+      singleGroupList = [rucksack]
+      groupCounter = 1
+  # force-add the last group of rucksacks
+  allGroupsList.append(singleGroupList)
+  return allGroupsList
+
+def getPriority(char):
+  asciiIndex = ord(char)
+  # The ASCII value of the lowercase alphabet is from 97 to 122
+  # The ASCII value of the uppercase alphabet is from 65 to 90.
+  # a value of 1-26 should be returned for lowercase letters - take ASCII value - 96
+  # a value of 27-52 should be returned for uppercase letters - take ASCII value - 38
+  if asciiIndex > 90:
+    priority = asciiIndex - 96
+  else:
+    priority = asciiIndex - 38
+  return priority
+
 def part1(input):
   totalPriority = 0
-
-  for rucksack in data:
+  for rucksack in input:
     # getting the common item
     compartment1, compartment2 = halveStr(rucksack)
     compartment1Set = set(compartment1)
@@ -17,22 +46,18 @@ def part1(input):
     commonItem = compartment1Set.intersection(compartment2Set)
     commonItem = list(commonItem)[0]
     
-    # assigning a priority to the common item
-    asciiIndex = ord(commonItem)
-    # The ASCII value of the lowercase alphabet is from 97 to 122
-    # The ASCII value of the uppercase alphabet is from 65 to 90.
-    # a value of 1-26 should be returned for lowercase letters - take ASCII value - 96
-    # a value of 27-52 should be returned for uppercase letters - take ASCII value - 38
-    if asciiIndex > 90:
-      priority = asciiIndex - 96
-    else:
-      priority = asciiIndex - 38
-    totalPriority += priority
-    # print(asciiIndex)
-  print(totalPriority) # 7878?
+    totalPriority += getPriority(commonItem)
+  print(totalPriority) # 7878
   return
 
 def part2(input):
+  groups = groupRucksacks(input)
+  badgePrioritySum = 0
+  for group in groups:
+    badges = set(group[0]).intersection(group[1]).intersection(group[2])
+    badges = list(badges)[0]
+    badgePrioritySum += getPriority(badges)
+  print(badgePrioritySum) # 2760
   return
 
 if __name__ == "__main__":
