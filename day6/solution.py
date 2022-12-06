b6519777b6b9def951a20567decda87fa87f402e
thereseLYR <thereselau1902@gmail.com>
2022-12-06 14:42:20 +0800

    day 6 part 1 solution

---
+++ day6/solution.py
@@ -0,0 +1,30 @@
+inputFile = open("day6\input.txt", 'r')
+data = inputFile.read()
+# print(data)
+
+# since the start-of character marker always has a fixed length (4 characters) we can use the sliding windows technique to minimize time complexity
+# within the 'viewport' of the algorithm, we further check if the 4 characters observed are all unique characters
+# this can be done by chucking the characters in a set and then ensuring that the length of the set is 4
+def findStartPacketMarker(signalStr):
+  markerFound, leftIndex, rightIndex = False, 0, 3
+  while(markerFound == False):
+    charSet = set(signalStr[leftIndex: rightIndex + 1])
+    if (len(charSet) == 4):
+      markerFound = True
+    leftIndex += 1
+    rightIndex += 1
+  return rightIndex, signalStr[leftIndex: rightIndex + 1]
+
+
+def part1(input):
+  position, marker = findStartPacketMarker(input)
+  print('testing input:', input)
+  print(position, marker)
+  return
+
+def part2(input):
+  return
+
+if __name__ == "__main__":
+    part1(data)
+    part2(data)
