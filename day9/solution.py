947bca5aa787cbbcf0ebe8d20689176cc6650638
thereseLYR <thereselau1902@gmail.com>
2022-12-09 23:30:58 +0800

    day 9 class-based solution for part 1

--- day9/solution.py
+++ day9/solution.py
@@ -1,92 +1,69 @@
 import copy
 
-inputFile = open("day9\input2.txt", 'r')
+inputFile = open("day9\input.txt", 'r')
 data = inputFile.read().split('\n')
 # print(data)
 
-# head and tail must always be adjacent = overlapping and diagonally adjacent also fulfil this criteria
-# instructions for the head are given as lines
-# figure out where the tail moves
-# size of the grid is unknown
-tailLocSet = set()
+parsedInstr = []
+for line in data:
+  parsedInstr.append(line.split(" "))
+# print(parsedInstr)
 
-def isDiagonal(head, tail):
-  relPosition = [tail[0] - head[0], tail[1] - head[1]]
-  if abs(relPosition[0]) + abs(relPosition[1]) == 2:
-    return True
-  else: return False
+movesDict= {"R": (1, 0), "U": (0, 1), "L": (-1, 0), "D": (0, -1)}
 
-def moveHeadR(head, tail):
-  # print("\ninitial head and tail:", head, tail)
-  if tail[0] < head[0]:
-    if head[1] != tail[1]:
-      tail[1] = head[1]
-    tail[0] += 1
-  head[0] += 1
-  # print("after move right:", head, tail)
-  return head, tail
+class Rope:
+  def __init__(self, knots):
+    self.knots = [(0, 0) for _ in range(knots)]
+    self.tailPositions = set()
+  
+  @property # define as property so that the head coordinates can be retrieved and updated by moveHead
+  def head(self):
+    return self.knots[0]
+  
+  # other than creating Rope as a class with its own properties and methods, 
+  # the other important part of this method is separating the head and tail movements
+  # the previous implementation hardcoded the movement of a single tail, which made it hard to scale to more knots
+  # instead, we now mathematically generalize the relative position of the tail from the head and then move it
+  def moveHead(self, dir):
+    d_x, d_y = movesDict[dir]
+    self.knots[0] = (self.head[0] + d_x, self.head[1] + d_y)
+  
+  def moveTail(self):
+    for index, knotCoords in enumerate(self.knots[1:]):
+      dxHead = self.knots[index][0] - knotCoords[0]
+      dyHead = self.knots[index][1] - knotCoords[1]
 
-def moveHeadL(head, tail):
-  # print("\ninitial head and tail:", head, tail)
-  if tail[0] > head[0]:
-    if head[1] != tail[1]:
-      tail[1] = head[1]
-    tail[0] -= 1
-  head[0] -= 1
-  # print("after move left:", head, tail)
-  return head, tail
-    
-def moveHeadUp(head, tail):
-  # print("\ninitial head and tail:", head, tail)
-  # check if the head and tail are in same col (based on x-coord)
-  if tail[1] < head[1]:
-    if head[0] != tail[0]:
-      tail[0] = head[0]
-    tail[1] += 1
-  head[1] += 1
-  # print("after move up:", head, tail)
-  return head, tail
+      # if knot is no longer adjacent or diagonally adjacent (dx or dy >1), it will need to be moved
+      if abs(dxHead) > 1 or abs(dyHead) > 1:
+        dxTail = dxHead if abs(dxHead == 1) else dxHead // 2
+        dyTail = dyHead if abs(dyHead == 1) else dyHead // 2
+        # change the position of the subsequent knot, i + 1
+        self.knots[index+1] = (knotCoords[0] + dxTail, knotCoords[1] + dyTail)
+        
+  def logTailPos(self):
+    # add the positon of the last knot to the set of positions
+    self.tailPositions.add(self.knots[-1])
 
-def moveHeadDown(head, tail):
-  # print("\ninitial head and tail:", head, tail)
-  if tail[1] > head[1]:
-    if head[0] != tail[0]:
-      tail[0] = head[0]
-    tail[1] -= 1
-  head[1] -= 1
-  # print("after move down:", head, tail)
-  return head, tail
-
-def moveOnce(direction, head, tail):
-  if direction == "R":
-    moveHeadR(head, tail)
-  elif direction == "L":
-    moveHeadL(head, tail)
-  elif direction == "U":
-    moveHeadUp(head, tail)
-  elif direction == "D":
-    moveHeadDown(head, tail)
-  return head, tail
+  def moveOnce(self, instr):
+    for _ in range(int(instr[1])): # instr[1]) is the number of steps to move in a direction
+      self.moveHead(instr[0]) # instr[0] is string that indicates the direction to move 
+      self.moveTail()
+      self.logTailPos()
+  
+  def moveAllInstr(self, instrList):
+    for instr in instrList:
+      self.moveOnce(instr)
 
 def part1(input):
-  # print("initial positions are:", headLoc, tailLoc)
-  headLoc, tailLoc = [0, 0], [0, 0]
-  for line in input:
-    [direction, steps] = line.split(" ")
-    for i in range(int(steps)):
-      moveOnce(direction, headLoc, tailLoc)
-      tailLocStr = str(tailLoc[0]) + str(tailLoc[1])
-      tailLocSet.add(tailLocStr)
-      # print(len(tailLocSet))
-
-  # print("final positions are:", headLoc, tailLoc)
-  # print("tail has visited:", tailLocSet)
-  print(len(tailLocSet)) # 6494
+  r = Rope(2)
+  r.moveAllInstr(input)
+  # print(r.tailPositions)
+  print(len(r.tailPositions)) # 6494
   return
 
 def part2(input):
   return
 
 if __name__ == "__main__":
-    part1(data)
-    part2(data)
+    part1(parsedInstr)
+    part2(parsedInstr)
